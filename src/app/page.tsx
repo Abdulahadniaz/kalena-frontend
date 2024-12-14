1ac84b79e920cd6d61c7af4dbf91cd7aa23b225f
Abdul Ahad <abdulahad1028@gmail.com>
2024-12-15 03:45:55 +0500

    styles for sm and md devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,31 +97,22 @@ export default function GridPage() {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-between items-center p-4 h-4 mt-2 ">
-        <div>
-          <p className="text-black">
+    <div className="flex flex-col min-h-screen p-2 sm:p-4 md:p-6">
+      <div className="flex flex-col sm:flex-row justify-between items-center mb-4 sm:mb-6">
+        <div className="mb-2 sm:mb-0">
+          <p className="text-sm sm:text-base text-black">
             Today:{" "}
-            {
-              <span className="italic text-gray-500 ">
-                {today.toDateString()}
-              </span>
-            }
-            {/* {!loading && (
-              <span className="italic text-gray-500 ">
-                {" - "}Number of upcoming events:
-              </span>
-            )} */}
+            <span className="italic text-gray-500">{today.toDateString()}</span>
           </p>
         </div>
-        <div className="flex items-center justify-between ">
+        <div className="flex items-center justify-between space-x-2 sm:space-x-4">
           <button
             onClick={goToPreviousMonth}
-            className="p-2 rounded-full hover:bg-gray-200"
+            className="p-1 sm:p-2 rounded-full hover:bg-gray-200"
           >
-            <ChevronLeftIcon className="w-6 h-6" />
+            <ChevronLeftIcon className="w-4 h-4 sm:w-6 sm:h-6" />
           </button>
-          <h2 className="text-xl font-bold">
+          <h2 className="text-lg sm:text-xl font-bold">
             {currentDate.toLocaleString("default", {
               month: "long",
               year: "numeric",
@@ -129,21 +120,21 @@ export default function GridPage() {
           </h2>
           <button
             onClick={goToNextMonth}
-            className="p-2 rounded-full hover:bg-gray-200"
+            className="p-1 sm:p-2 rounded-full hover:bg-gray-200"
           >
-            <ChevronRightIcon className="w-6 h-6" />
+            <ChevronRightIcon className="w-4 h-4 sm:w-6 sm:h-6" />
           </button>
         </div>
       </div>
-      <div className="p-4 sm:p-6 lg:p-4">
-        <div className="grid grid-cols-7 gap-2 h-full">
+      <div className="flex-grow">
+        <div className="grid grid-cols-7 gap-1 sm:gap-2 h-full">
           {/* Day headings */}
           {daysOfWeek.map((day) => (
             <div
               key={day}
-              className={`text-center font-semibold text-gray-700 pb-2`}
+              className="text-center font-semibold text-gray-700 pb-1 sm:pb-2 text-xs sm:text-sm"
             >
-              {day}
+              {day.slice(0, 3)}
             </div>
           ))}
 
@@ -157,18 +148,19 @@ export default function GridPage() {
             return (
               <div
                 key={index}
-                className={`rounded-lg shadow-md flex flex-col items-center justify-start p-2 aspect-[12/6] overflow-hidden
+                className={`rounded-lg shadow-sm flex flex-col items-center justify-start p-1 sm:p-2 sm:aspect-square xl:aspect-[12/6] overflow-hidden
                 ${isWeekend ? "bg-gray-200" : "bg-white"}
                 ${index % 7 === 6 ? "border-r-0" : ""}
-                ${Math.floor(index / 7) === weeks - 1 ? "border-b-0" : ""}`}
+                ${Math.floor(index / 7) === weeks - 1 ? "border-b-0" : ""}
+                ${isCurrentMonth ? "cursor-pointer " : ""}`}
                 onClick={() => handleCellClick(dayNumber)}
               >
                 <span
-                  className={`text-gray-600 font-light mb-1 ${
-                    isCurrentMonth ? "" : "opacity-30"
-                  } ${
+                  className={`text-xs sm:text-sm font-light mb-1 
+                  ${isCurrentMonth ? "" : "opacity-30"}
+                  ${
                     isTodayDate
-                      ? "bg-blue-500 text-white px-2 py-1 rounded-full"
+                      ? "bg-blue-500 text-white px-1 sm:px-2 py-0.5 sm:py-1 rounded-full"
                       : ""
                   }`}
                 >
